53de824eca45c5819f23fec3026d74124f6fb7a8
AlRoman747 <romaaltahir@icloud.com>
2025-09-28 16:26:08 +0300

    done

---
+++ flatten.py
@@ -0,0 +1,14 @@
+lst = [1, [2, 3, [4, 5]], (6, 7), "abc", ["pwkdw", [8, 9]], [[], [[0,[9]],[[[]]]]]]
+
+def flatten(nested):
+    res = []
+    stack = nested[::-1]
+    while stack:
+        elem = stack.pop()
+        if type(elem) in [list,tuple,set,frozenset]:
+            stack.extend(elem[::-1])
+        else:
+            res.append(elem)
+    return res
+
+print(flatten(lst))
