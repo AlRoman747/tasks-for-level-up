48dba74c61d6aac2a1fff90f7afff8cb844e5f70
AlRoman747 <romaaltahir@icloud.com>
2025-09-28 17:28:02 +0300

    проверка типов

--- flatten.py
+++ flatten.py
@@ -1,6 +1,8 @@
 lst = [1, [2, 3, [4, 5]], (6, 7), "abc", ["pwkdw", [8, 9]], [[], [[0,[9]],[[[]]]]]]
 
 def flatten(nested):
+    if type(nested) != list:
+        return "ERR0R"
     res = []
     stack = nested[::-1]
     while stack:
